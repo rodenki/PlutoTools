e232e16856f1e5b500394d0de7a788b1cc7d84fd
Peter Rodenkirch <p.rodenkirch@gmail.com>
2017-10-08 17:31:50 +0200

    corrected magnetic field issue

--- simulationData.py
+++ simulationData.py
@@ -5,7 +5,6 @@ import h5py
 import numpy as np
 import scipy
 from scipy.ndimage.interpolation import map_coordinates
-from scipy import interpolate
 from scipy import stats
 import xml.etree.cElementTree as xml
 from copy import deepcopy
@@ -88,7 +87,8 @@ class SimulationData:
             self.variables["bx1"] = np.array(self.hdf5File[self.timestep]['vars']['bx1'])
             self.variables["bx2"] = np.array(self.hdf5File[self.timestep]['vars']['bx2'])
         except KeyError:
-            print("no magnetic field present")
+            print("Magnetic field not present.")
+
         self.hdf5File.close()
 
         xmlPath = self.filename[:-2] + "xmf"
@@ -342,13 +342,20 @@ class Tools:
         return x, y
 
     @staticmethod
-    def plotVariable(data, variable, filename="data", log=True, show=False, clear=True):
+    def plotVariable(data, variable, filename="data", log=True, show=False,
+                     clear=True, interpolate=False, resolution=1000):
         x, y = Tools.polarCoordsToCartesian(data.x1, data.x2)
-        plt.figure(figsize=(10, 8))
+        plt.figure(figsize=(10, 7))
+
+        if interpolate:
+            ranges = [np.min(x), np.max(y), resolution]
+            x, y, variable = Tools.interpolateToUniformGrid(data, variable, ranges, ranges)
+
         if log:
-            plt.pcolormesh(x, y, variable, norm=LogNorm(vmin=variable.min(), vmax=variable.max()), cmap=cm.inferno)
+            plt.pcolormesh(x, y, variable, norm=LogNorm(vmin=np.nanmin(variable), vmax=np.nanmax(variable)), cmap=cm.inferno)
         else:
             plt.pcolormesh(x, y, variable, cmap=cm.inferno)
+
         plt.colorbar()
         plt.xlabel(r'r')
         plt.ylabel(r'z')
@@ -361,11 +368,15 @@ class Tools:
             plt.close()
 
     @staticmethod
-    def plotDensity(data, filename="dens", show=False, clear=True):
+    def plotDensity(data, filename="dens", show=False, clear=True,
+                    interpolate=False, resolution=1000):
         x, y = Tools.polarCoordsToCartesian(data.x1, data.x2)
         plt.figure(figsize=(10, 7))
         rho = data.variables["rho"]
-        plt.pcolormesh(x, y, rho, norm=LogNorm(vmin=rho.min(), vmax=rho.max()), cmap=cm.inferno)
+        if interpolate:
+            ranges = [np.min(x), np.max(y), resolution]
+            x, y, rho = Tools.interpolateToUniformGrid(data, rho, ranges, ranges)
+        plt.pcolormesh(x, y, rho, norm=LogNorm(vmin=np.nanmin(rho), vmax=np.nanmax(rho)), cmap=cm.inferno)
         plt.colorbar()
         plt.xlabel(r'r')
         plt.ylabel(r'z')
@@ -458,11 +469,14 @@ class Tools:
                           norm=True):
 
         Tools.transformMagneticFieldToCylindrical(data)
-        Tools.interpolateRadialGrid(data, np.linspace(0.4, 98.5, 500))
+        # Tools.interpolateRadialGrid(data, np.linspace(0.4, 98.5, 500))
         x, y = Tools.polarCoordsToCartesian(data.x1, data.x2)
-
         bx1 = data.variables["bx1"]
         bx2 = data.variables["bx2"]
+
+        ranges = [np.min(x), np.max(y), 100]
+        x, y, bx1 = Tools.interpolateToUniformGrid(data, bx1, ranges, ranges)
+        x, y, bx2 = Tools.interpolateToUniformGrid(data, bx2, ranges, ranges)
 
         if norm:
             n = np.sqrt(bx1**2 + bx2**2)
@@ -470,12 +484,41 @@ class Tools:
             bx2 /= n
 
         # plt.figure(figsize=(10, 7))
-        for r in range(x1_start, len(data.x1), dx1):
-            plt.quiver(x[:,r][::dx2],
-                       y[:,r][::dx2],
-                       bx1[:,r][::dx2],
-                       bx2[:,r][::dx2],
-                       width=width, scale=scale, color='k')
+        plt.quiver(x, y, bx1, bx2,
+                   width=width, scale=scale, color='k')
+
+        if show:
+            plt.show()
+        else:
+            plt.savefig(filename + ".png", dpi=400)
+
+        if clear:
+            plt.cla()
+            plt.close()
+
+    @staticmethod
+    def plotMagneticFieldLines(data, filename="mag_fieldlines", dx1=10, dx2=5, scale=40,
+                          width=0.001, x1_start=0, clear=True, show=False,
+                          norm=True):
+
+        Tools.transformMagneticFieldToCylindrical(data)
+        # Tools.interpolateRadialGrid(data, np.linspace(0.4, 98.5, 500))
+        x, y = Tools.polarCoordsToCartesian(data.x1, data.x2)
+        bx1 = data.variables["bx1"]
+        bx2 = data.variables["bx2"]
+
+        ranges = [np.min(x), np.max(y), 1000]
+        x, y, bx1 = Tools.interpolateToUniformGrid(data, bx1, ranges, ranges)
+        x, y, bx2 = Tools.interpolateToUniformGrid(data, bx2, ranges, ranges)
+
+        if norm:
+            n = np.sqrt(bx1**2 + bx2**2)
+            bx1 /= n
+            bx2 /= n
+
+        # plt.figure(figsize=(10, 7))
+        plt.streamplot(x, y, bx1, bx2, density=2, arrowstyle='->', linewidth=1,
+                       arrowsize=1.5)
 
         if show:
             plt.show()
@@ -520,8 +563,19 @@ class Tools:
             interpolated = np.array(np.zeros(shape=(value.shape[0], len(newTicks))))
 
             for i in range(value.shape[0]):
-                f = interpolate.interp1d(x1, value[i])
+                f = scipy.interpolate.interp1d(x1, value[i])
                 interpolated[i] = f(newTicks)
 
             data.variables[key] = interpolated
         data.x1 = newTicks
+
+    @staticmethod
+    def interpolateToUniformGrid(data, variable, x_range, y_range):
+        x, y = Tools.polarCoordsToCartesian(data.x1, data.x2)
+        x = np.ravel(x)
+        y = np.ravel(y)
+        variable = np.ravel(variable)
+        points = np.column_stack((x, y))
+        grid_x, grid_y = np.meshgrid(np.linspace(*x_range), np.linspace(*y_range))
+        newVariable = scipy.interpolate.griddata(points, variable, (grid_x, grid_y))
+        return grid_x, grid_y, newVariable
